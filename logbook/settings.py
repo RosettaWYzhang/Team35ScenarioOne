a3a2b1aefa60bc84ca617f7bbc180294732505f6
Ubuntu <team35admin@Team35VM.dmbgwa0f1voend3xurpm5icnke.zx.internal.cloudapp.net>
2017-10-31 23:55:30 +0000

    update hosts

--- logbook/settings.py
+++ logbook/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i%06y2q&4l-!nv*8oolv470b!o)!xg*^9f7^d=q10#b$wd%c_e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['team35webapp.azurewebsites.net', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['team35webapp.azurewebsites.net']
 DEBUG = False
 
 # Application definition
