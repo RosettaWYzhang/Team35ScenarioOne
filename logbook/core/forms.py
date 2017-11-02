a2d0257130fe5c733e13a7f4832907a3e422a571
Wanyue <RosettaWYzhang@users.noreply.github.com>
2017-11-02 16:05:59 +0000

    logbook with more functionalities

--- logbook/core/forms.py
+++ logbook/core/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from logbook.core.models import Entry, Category
 
 
 class SignUpForm(UserCreationForm):
@@ -11,3 +13,14 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ('title', 'slug', 'body', 'url', 'category',)
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ('title', 'slug',)
+
